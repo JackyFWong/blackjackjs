33fd5cc7efe51b65e00651ea92980f3d8a203b70
JackyFWong <jwong2398@gmail.com>
2019-04-24 00:39:20 -0400

    new changes

--- main.js
+++ main.js
@@ -11,18 +11,19 @@ var hand = [];
 var handScore = 0;
 var suits = ["c", "d", "h", "s"];
 
+function addPoints(score, card) {
+    if (typeof card == "number")
+        score += card;
+    else
+        score += 10;
+}
+
 // true if enemy draws again
 // false if enemy stops
 function drawChance() {
-    var enemyTotal = 0;
-    for (var card in enemy) {
-        if (enemy.hasOwnProperty(card)) {
-            enemyTotal += parseFloat(enemy[card]);
-        }
-    }
     // get a random integer from 0-total of enemy points
-    var chance = Math.floor(Math.random() * enemyTotal);
-    if (chance % enemyTotal === 1) { return true; }
+    var chance = Math.floor(Math.random() * enemyScore);
+    if (chance % enemyScore === 1) { return true; }
     else { return false; }
 }
 
@@ -32,7 +33,7 @@ function drawAction(player) {
     var curCard = player.push(deck.splice(ind, 1));
 
     if (player === "enemy") {
-        enemyScore += curCard[1];
+        addPoints(enemyScore, curCard[1]);
         if (enemyScore > 21) {
             window.alert("Enemy has lost! Over 21");
         }
@@ -43,15 +44,15 @@ function drawAction(player) {
         }
     }
     else {
-        handScore += curCard[1];
+        addPoints(handScore, curCard[1]);
         if (handScore > 21) {
             window.alert("You have lost! Over 21");
         }
         else {
             // update cards display
             var cardsPlayer = "";
-            for (var cards in player) {
-                cardsPlayer = cardsPlayer + " " + cards[0] + " " + cards[1];
+            for (var i = 0; i < hand.length; i++) {
+                cardsPlayer = cardsPlayer + " " + hand[i][0];
             }
             document.getElementById("info").innerHTML = "Your cards: " + cardsPlayer;
         }
@@ -62,14 +63,14 @@ function start() {
     // reset and build deck
     deck = [];
     var ind = 0;
-    for (var i in suits) {
+    for (var i = 0; i < 4; i++) {
         for (var j = 1; j <= 10; j++) {
-            deck[ind++] = [i + " " +j, j];
+            deck[ind++] = [suits[i], j];
         }
         // add jack queen and king
-        deck[ind++] = [i+" j", 10];
-        deck[ind++] = [i+" q", 10];
-        deck[ind++] = [i+" k", 10];
+        deck[ind++] = [suits[i], "j"];
+        deck[ind++] = [suits[i], "q"];
+        deck[ind++] = [suits[i], "k"];
     }
 
     // deal two cards to players
@@ -79,11 +80,11 @@ function start() {
     drawAction(enemy);
 
     // update html
-    var cardsPlayer = hand[0][0] + " " + hand[1][0];
+    var cardsPlayer = hand[0][0] + " | " + hand[1][0];
     document.getElementById("info").innerHTML = "Your cards: " + cardsPlayer;
 
     // this never updates as you dont get to see new enemy cards
-    var cardsEnemy = enemy[0][0];
+    var cardsEnemy = enemy[0][0] + enemy[0][1];
     document.getElementById("enemy").innerHTML = "Enemy faceup card: " + cardsEnemy;
 }
 
